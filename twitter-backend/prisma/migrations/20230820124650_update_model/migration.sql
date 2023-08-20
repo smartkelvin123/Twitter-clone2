4f6c0747468dec728847bdc7a9aecf1341c1d51d
unknown <smartkelvin66@gmail.com>
2023-08-20 16:31:45 +0100

    added auth

---
+++ twitter-backend/prisma/migrations/20230820124650_update_model/migration.sql
@@ -0,0 +1,69 @@
+/*
+  Warnings:
+
+  - You are about to drop the `user` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropIndex
+DROP INDEX "user_username_key";
+
+-- DropIndex
+DROP INDEX "user_email_key";
+
+-- DropTable
+PRAGMA foreign_keys=off;
+DROP TABLE "user";
+PRAGMA foreign_keys=on;
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "email" TEXT NOT NULL,
+    "username" TEXT,
+    "image" TEXT,
+    "bio" TEXT,
+    "name" TEXT,
+    "isVerified" BOOLEAN NOT NULL DEFAULT false
+);
+
+-- CreateTable
+CREATE TABLE "Token" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "type" TEXT NOT NULL,
+    "emailToken" TEXT,
+    "valid" BOOLEAN NOT NULL DEFAULT true,
+    "expiration" DATETIME NOT NULL,
+    "userId" INTEGER NOT NULL,
+    CONSTRAINT "Token_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Tweet" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "content" TEXT NOT NULL,
+    "image" TEXT,
+    "impression" INTEGER NOT NULL DEFAULT 0,
+    "userId" INTEGER NOT NULL,
+    CONSTRAINT "Tweet_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_Tweet" ("content", "createdAt", "id", "image", "impression", "updatedAt", "userId") SELECT "content", "createdAt", "id", "image", "impression", "updatedAt", "userId" FROM "Tweet";
+DROP TABLE "Tweet";
+ALTER TABLE "new_Tweet" RENAME TO "Tweet";
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Token_emailToken_key" ON "Token"("emailToken");
--- twitter-backend/src/index.ts
+++ twitter-backend/src/index.ts
@@ -1,13 +1,15 @@
 import express from "express";
 import userRoutes from "./routes/userRoutes";
 import tweetRoutes from './routes/tweetRoutes'
+import authRoutes from "./routes/authRoutes"
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
-app.use('/user',  userRoutes); 
+app.use("/user",  userRoutes); 
 app.use ("/tweet", tweetRoutes)
+app.use("/auth", authRoutes)
 
 app.get('/', (req, res) => {
     res.send('Hello "updated" world!');
